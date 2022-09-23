81d18923bad06ba32f838811970c6ace46b3c7bc
小橙子 <a2742217795@hotmail.com>
2022-09-23 15:42:53 +0800

    2022-9-23

--- demo.ts
+++ demo.ts
@@ -3,26 +3,34 @@
 
 type array = [1, 2, 3, 4, 5, 6]
 // 检索 数组
-type IsSameValue<A, B> = (A extends B ? true : false) & (B extends A ? true : false)
+type IsSameValue<A, B> = (A extends B ? true : false) &
+  (B extends A ? true : false)
 
-type IncludeArray<Arr extends unknown[], Target> = Arr extends [infer First, ...infer Rest]
+type IncludeArray<Arr extends unknown[], Target> = Arr extends [
+  infer First,
+  ...infer Rest
+]
   ? IsSameValue<First, Target> extends true
     ? true
     : IncludeArray<Rest, Target>
   : false
 
 // 返回查到的数据
-type SerchArray<Arr extends unknown[], Target> = Arr extends [infer First, ...infer Rest]
+type SerchArray<Arr extends unknown[], Target> = Arr extends [
+  infer First,
+  ...infer Rest
+]
   ? IsSameValue<First, Target> extends true
     ? First
     : SerchArray<Rest, Target>
   : never
 
 // 删除匹配到的数组元素
-type deleteArrayElement<Arr extends unknown[], Target, Result extends unknown[] = []> = Arr extends [
-  infer First,
-  ...infer Rest
-]
+type deleteArrayElement<
+  Arr extends unknown[],
+  Target,
+  Result extends unknown[] = []
+> = Arr extends [infer First, ...infer Rest]
   ? IsSameValue<First, Target> extends true
     ? deleteArrayElement<Rest, Target, Result>
     : deleteArrayElement<Rest, Target, [...Result, First]>
@@ -31,18 +39,113 @@ type deleteArrayElement<Arr extends unknown[], Target, Result extends unknown[] 
 type DeleteArray = deleteArrayElement<array, 5>
 
 // 构造长度不定的数组
-type ChangeArray<Num extends number, El = unknown, Arr extends unknown[] = []> = Arr['length'] extends Num
-  ? Arr
-  : ChangeArray<Num, El, [...Arr, El]>
+type ChangeArray<
+  Num extends number,
+  El = unknown,
+  Arr extends unknown[] = []
+> = Arr['length'] extends Num ? Arr : ChangeArray<Num, El, [...Arr, El]>
 
 type changeArray = ChangeArray<10>
 
-type Camelcase<Str extends string> = Str extends `${infer Left}_${infer Right}${infer Rest}`
-  ? `${Left}${Uppercase<Right>}${Camelcase<Rest>}`
-  : Str
+type Camelcase<Str extends string> =
+  Str extends `${infer Left}_${infer Right}${infer Rest}`
+    ? `${Left}${Uppercase<Right>}${Camelcase<Rest>}`
+    : Str
 
 // 处理数组中字符串将其遇到 '_' 后的字符大写
-type CamelcaseArr<Arr extends unknown[]> = Arr extends [infer First, ...infer Rest]
+type CamelcaseArr<Arr extends unknown[]> = Arr extends [
+  infer First,
+  ...infer Rest
+]
   ? [Camelcase<First & string>, ...CamelcaseArr<Rest>]
   : []
 type camelcaseArr = CamelcaseArr<['aaa_aaa', 'bb_bb_bb', 'cc_cc_cc']>
+
+// ---------------------联合类型的问题-----------------------
+
+// 问题1
+type Test<T> = T extends number ? 1 : 2
+// 输出什么的？
+type testValueDateReturn = Test<1 | 'a'> //  => 1 | 2
+
+// 问题2：
+type TestBoolean<T> = T extends true ? 1 : 2
+// 输出什么的？
+type TestValueBoolean = TestBoolean<boolean> //  => 1 | 2
+
+// 问题3:
+type TestAnyValueReturn<T> = T extends true ? 1 : 2
+// 输出什么
+type testAnyValueReturn = TestAnyValueReturn<any> //  => 1 | 2
+
+/**
+ * 原因：
+ * 条件类型对 any 做了特殊处理，
+ * 如果左边是 any，那么直接把 trueType 和 falseType 合并成联合类型返回。
+ */
+
+// 问题3:
+type TestInputNeverReturn<T> = T extends true ? 1 : 2
+// 输出什么
+type testImputNeverReturn = TestInputNeverReturn<never> //  => never
+
+/**
+ * 原因：
+ *  TS 的特殊处理，当条件类型左边是 never 时，直接返回 never。
+ */
+
+// 文章 ts 类型问题 https://juejin.cn/post/7066745410194243597?share_token=324476fb-1e6b-45da-93a4-cf7041ed9c73
+
+interface Person {
+  name: string
+  age: number
+}
+
+interface Guang {
+  name: string
+  age: number
+  hobbies: string[]
+}
+
+let person: Person = {
+  name: '',
+  age: 20
+}
+
+let guang: Guang = {
+  name: 'guang',
+  age: 20,
+  hobbies: ['play game', 'writing']
+}
+
+// 产生了协变
+person = guang
+
+let printHobbies: (guang: Guang) => void
+
+printHobbies = (guang) => {
+  console.log(guang.hobbies)
+}
+
+let printName: (person: Person) => void
+
+printName = (person) => {
+  console.log(person.name)
+}
+
+// 产生了逆变
+printHobbies = printName
+
+// 虽然 这里 printName 使用到了父类型 但是也符合子类型的约束
+// 所以这里并没问题
+
+/**
+ * 如何理解这句话 函数的参数具有逆变性质，而返回值是协变的
+ */
+
+// 当父类型可以赋值给子类型时产生了逆变，子类型赋值给父类型时产生了协变
+// 两者都能发生，就叫做双向协变
+printName = printHobbies
+
+// 双向协变 存在于 ts 2x 版本中需要通过 strictFunctionTypes 来去管理
+// ture 支持函数参数的逆变，设置为 fasle 则是双向协变
--- index.ts
+++ index.ts
@@ -3,8 +3,6 @@
  * 索引可以做出变化，用 as 操作符，叫重映射
  */
 
-import { RestTypeNode } from './node_modules/typescript/lib/typescript'
-
 // ====模式匹配
 
 // ----数组
@@ -12,25 +10,40 @@ type GetValueResult<P> = P extends Array<infer Value> ? Value : never
 type value = GetValueResult<number[]>
 
 // 提取数组类型的第一个元素的类型
-type GetArrayFirst<T extends unknown[]> = T extends [infer Value, ...unknown[]] ? Value : never
+type GetArrayFirst<T extends unknown[]> = T extends [infer Value, ...unknown[]]
+  ? Value
+  : never
 type FirstArray = GetArrayFirst<[1, 2, 3]>
 
 // 提取最后一个元素
-type GetArrayLast<T extends unknown[]> = T extends [...unknown[], infer Value] ? Value : never
+type GetArrayLast<T extends unknown[]> = T extends [...unknown[], infer Value]
+  ? Value
+  : never
 type LastArray = GetArrayLast<[1, 2, 3]>
 
 // 取出排除最后一个的剩余数组
-type GetArray<T extends unknown[]> = T extends [] ? [] : T extends [...infer Value, unknown] ? Value : unknown
+type GetArray<T extends unknown[]> = T extends []
+  ? []
+  : T extends [...infer Value, unknown]
+  ? Value
+  : unknown
 type getArray = GetArray<[1, 2, 3]>
 
 // 取出排除第一个的剩余数组
-type GetArrayShift<T extends unknown[]> = T extends [] ? [] : T extends [unknown, ...infer Value] ? Value : unknown
+type GetArrayShift<T extends unknown[]> = T extends []
+  ? []
+  : T extends [unknown, ...infer Value]
+  ? Value
+  : unknown
 type ShiftValue = GetArrayShift<[1, 2, 3]>
 
 // ----字符串
 
 // 判断字符串是否以某个字符为开头
-type StartWith<T extends string, Prefix extends string> = T extends `${Prefix}${string}` ? true : false
+type StartWith<
+  T extends string,
+  Prefix extends string
+> = T extends `${Prefix}${string}` ? true : false
 type IsStartWith = StartWith<'value-number', 'value'>
 
 // 字符串替换
@@ -38,16 +51,28 @@ type ReplaceStr<
   Str extends string,
   From extends string,
   To extends string
-> = Str extends `${infer Prefix}${From}${infer Last}` ? `${Prefix}${To}${Last}` : Str
+> = Str extends `${infer Prefix}${From}${infer Last}`
+  ? `${Prefix}${To}${Last}`
+  : Str
 type rePlaceStr = ReplaceStr<'valuegetnumber', 'get', 'ww'>
 
 // 取消字符串尾部的空白字符串
-type TrimStrRight<Str extends string> = Str extends `${infer Rest}${' ' | '\n' | '\t'}` ? TrimStrRight<Rest> : Str
+type TrimStrRight<Str extends string> = Str extends `${infer Rest}${
+  | ' '
+  | '\n'
+  | '\t'}`
+  ? TrimStrRight<Rest>
+  : Str
 // 这里值得注意的是使用了递归处理
 type trimStateRight = TrimStrRight<`dsdsds     `>
 
 // 取消字符串前面的空白字符串
-type TrimStrLeft<Str extends string> = Str extends `${' ' | '\n' | '\t'}${infer Rest}` ? TrimStrLeft<Rest> : Str
+type TrimStrLeft<Str extends string> = Str extends `${
+  | ' '
+  | '\n'
+  | '\t'}${infer Rest}`
+  ? TrimStrLeft<Rest>
+  : Str
 type trimStrLeft = TrimStrLeft<`    dsdsdsds`>
 
 // 这两者何必 形成 trim() 用法
@@ -57,11 +82,19 @@ type trimStr = TrimStr<`    dsdsds    `>
 // ----函数
 
 // 提取参数
-type GetParameters<Func extends Function> = Func extends (...args: infer Args) => unknown ? Args : never
+type GetParameters<Func extends Function> = Func extends (
+  ...args: infer Args
+) => unknown
+  ? Args
+  : never
 type getParameters = GetParameters<(name: string, age: number) => string>
 
 // 提取返回类型
-type GetReturnType<Func extends Function> = Func extends (...args: any[]) => infer ReturnType ? ReturnType : never
+type GetReturnType<Func extends Function> = Func extends (
+  ...args: any[]
+) => infer ReturnType
+  ? ReturnType
+  : never
 type getReturnType = GetReturnType<(name: string, age: number) => string>
 
 // 确保对象上函数在执行[call\bind\apply]时 this 指向是否正确
@@ -76,7 +109,12 @@ const dong = new Dong()
 // dong.hello.call({name: 'value'})
 
 // 提取 this
-type GetThisParameterType<T> = T extends (this: infer ThisType, ...args: any[]) => any ? ThisType : unknown
+type GetThisParameterType<T> = T extends (
+  this: infer ThisType,
+  ...args: any[]
+) => any
+  ? ThisType
+  : unknown
 type getThisParameterType = GetThisParameterType<typeof dong.hello>
 
 // ====构造器
@@ -91,19 +129,17 @@ interface PersonConstructor {
 }
 
 // 提取构造函数的返回对象
-type GetInstanceType<Constructor extends new (...res: any) => any> = Constructor extends new (
-  ...res: any
-) => infer InstanceType
-  ? InstanceType
-  : any
+type GetInstanceType<Constructor extends new (...res: any) => any> =
+  Constructor extends new (...res: any) => infer InstanceType
+    ? InstanceType
+    : any
 type getInstanceType = GetInstanceType<PersonConstructor>
 
 //提取构造函数的参数类型
-type GetParameterConstructor<Constructor extends new (...args: any) => any> = Constructor extends new (
-  ...args: infer ParametersType
-) => unknown
-  ? ParametersType
-  : never
+type GetParameterConstructor<Constructor extends new (...args: any) => any> =
+  Constructor extends new (...args: infer ParametersType) => unknown
+    ? ParametersType
+    : never
 type getParametersConstructor = GetParameterConstructor<PersonConstructor>
 
 // ====索引类型
@@ -137,7 +173,10 @@ type tupleUnShift = UnShift<tuple, 0>
 
 //[1,2] + ['guang', 'dong'] => [[1, 'guang'], [2, 'dong']]
 // 递归去处理 能处理数量不定的数组
-type Zip2<One extends unknown[], Other extends unknown[]> = One extends [infer OneFirst, ...infer OneRest]
+type Zip2<One extends unknown[], Other extends unknown[]> = One extends [
+  infer OneFirst,
+  ...infer OneRest
+]
   ? Other extends [infer OtherFirst, ...infer OtherRest]
     ? [[OneFirst, OtherFirst], ...Zip2<OneRest, OtherRest>]
     : []
@@ -148,17 +187,22 @@ type zip2 = Zip2<[1, 2, 3, 4, 5], ['A', 'B', 'C', 'D', 'E']>
 // ----字符串类型的重新构造
 
 // 首字母大写
-type CapitalizerStr<Str extends string> = Str extends `${infer First}${infer Rest}` ? `${Uppercase<First>}${Rest}` : Str
+type CapitalizerStr<Str extends string> =
+  Str extends `${infer First}${infer Rest}` ? `${Uppercase<First>}${Rest}` : Str
 type capitalizerStr = CapitalizerStr<'hello'>
 
 // 下划线转驼峰
-type CamelCase<Str extends string> = Str extends `${infer Left}_${infer Right}${infer Rest}`
-  ? `${Left}${Uppercase<Right>}${CamelCase<Rest>}`
-  : Str
+type CamelCase<Str extends string> =
+  Str extends `${infer Left}_${infer Right}${infer Rest}`
+    ? `${Left}${Uppercase<Right>}${CamelCase<Rest>}`
+    : Str
 type camelCase = CamelCase<'dong_dong_dong'>
 
 // 删除匹配字符
-type DropSubStr<Str extends string, SubStr extends string> = Str extends `${infer Prefix}${SubStr}${infer Suffix}`
+type DropSubStr<
+  Str extends string,
+  SubStr extends string
+> = Str extends `${infer Prefix}${SubStr}${infer Suffix}`
   ? DropSubStr<`${Prefix}${Suffix}`, SubStr>
   : Str
 type dropSubStr = DropSubStr<'dong~~~~~', '~'>
@@ -166,10 +210,15 @@ type dropSubStr = DropSubStr<'dong~~~~~', '~'>
 // ---- 函数
 
 // 函数类型的重新构造
-type AppendArgument<Func extends Function, Arg> = Func extends (...args: infer Args) => infer ReturnType
+type AppendArgument<Func extends Function, Arg> = Func extends (
+  ...args: infer Args
+) => infer ReturnType
   ? (...args: [Args, Arg]) => ReturnType
   : never
-type appendArgument = AppendArgument<(name: string, args: number) => string, number>
+type appendArgument = AppendArgument<
+  (name: string, args: number) => string,
+  number
+>
 
 // ----索引类型
 
@@ -215,13 +264,13 @@ type FilterByValueType<Obj extends Record<string, any>, ValueType> = {
   [Key in keyof Obj as Obj[Key] extends ValueType ? Key : never]: Obj[Key]
 }
 
-interface Person {
+interface Personsssss {
   name: string
   age: number
   hobby: string[]
 }
 
-type filterByValueType = FilterByValueType<Person, string | number>
+type filterByValueType = FilterByValueType<Personsssss, string | number>
 
 /**
  * 总结： 想要类型变换的就要重新构造，重新构造的时候可以对其过滤、修改、变换
@@ -237,19 +286,29 @@ type filterByValueType = FilterByValueType<Person, string | number>
 type ttt = Promise<Promise<Promise<Record<string, any>>>>
 
 // 通过递归提取一个不确定层数的 promise 中的 value
-type DeepPromiseValueType<T> = T extends Promise<infer ValueType> ? DeepPromiseValueType<ValueType> : T
+type DeepPromiseValueType<T> = T extends Promise<infer ValueType>
+  ? DeepPromiseValueType<ValueType>
+  : T
 type deepPromiseValueType = DeepPromiseValueType<ttt>
 
 // ----数组的递归
 
 // 倒序
 type arr = [1, 2, 3, 4, 5]
-type ReversArr<T extends Array<unknown>> = T extends [...infer Firsts, infer Last] ? [Last, ...ReversArr<Firsts>] : T
+type ReversArr<T extends Array<unknown>> = T extends [
+  ...infer Firsts,
+  infer Last
+]
+  ? [Last, ...ReversArr<Firsts>]
+  : T
 type reversArr = ReversArr<arr>
 
 // 查找元素
 type IsEqual<A, B> = (A extends B ? true : false) & (B extends A ? true : false)
-type Includes<Arr extends unknown[], FindItem> = Arr extends [infer First, ...infer Rest]
+type Includes<Arr extends unknown[], FindItem> = Arr extends [
+  infer First,
+  ...infer Rest
+]
   ? IsEqual<First, FindItem> extends true
     ? true
     : Includes<Rest, FindItem>
@@ -259,7 +318,11 @@ type IncludesResult = Includes<[1, 2, 3, 4, 5], 4>
 type IncludesResult2 = Includes<[1, 2, 3, 4, 5], 6>
 
 // 在数组中删除与之匹配的元素
-type RemoveItem<Arr extends unknown[], Item, Result extends unknown[]> = Arr extends [infer First, ...infer Rest]
+type RemoveItem<
+  Arr extends unknown[],
+  Item,
+  Result extends unknown[]
+> = Arr extends [infer First, ...infer Rest]
   ? IsEqual<First, Item> extends true
     ? RemoveItem<Rest, Item, Result>
     : RemoveItem<Rest, Item, [...Result, First]>
@@ -267,9 +330,11 @@ type RemoveItem<Arr extends unknown[], Item, Result extends unknown[]> = Arr ext
 type removeItem = RemoveItem<[1, 2, 2, 2, 2, 2, 3, 4, 5], 2, []>
 
 // 创建“不定”数组 当给定的数组类型元素不确定时候，就需要递归处理
-type BuildArray<Length extends number, Ele = unknown, Arr extends unknown[] = []> = Arr['length'] extends Length
-  ? Arr
-  : BuildArray<Length, Ele, [...Arr, Ele]>
+type BuildArray<
+  Length extends number,
+  Ele = unknown,
+  Arr extends unknown[] = []
+> = Arr['length'] extends Length ? Arr : BuildArray<Length, Ele, [...Arr, Ele]>
 type buildArray = BuildArray<5, 'number', []>
 
 // ----字符串类型的递归
@@ -279,17 +344,23 @@ type ReplaceStrAll<
   Str extends string,
   From extends string,
   To extends string
-> = Str extends `${infer Prefix}${From}${infer Suffix}` ? `${Prefix}${To}${ReplaceStrAll<Suffix, From, To>}` : Str
+> = Str extends `${infer Prefix}${From}${infer Suffix}`
+  ? `${Prefix}${To}${ReplaceStrAll<Suffix, From, To>}`
+  : Str
 type replaceStrAll = ReplaceStrAll<'AnnnBercerrWW', 'Ber', 'GGGG'>
 
 // 将字符串每一个字符设置为联合类型
-type StringToUnion<Str extends string> = Str extends `${infer First}${infer Value}`
-  ? First | StringToUnion<Value>
-  : never
+type StringToUnion<Str extends string> =
+  Str extends `${infer First}${infer Value}`
+    ? First | StringToUnion<Value>
+    : never
 type stringToUnion = StringToUnion<'value'>
 
 // 字符串的反转
-type ReversStr<Str extends string, Result extends string = ''> = Str extends `${infer First}${infer Rest}`
+type ReversStr<
+  Str extends string,
+  Result extends string = ''
+> = Str extends `${infer First}${infer Rest}`
   ? ReversStr<Rest, `${First}${Result}`>
   : Result
 type reversStr = ReversStr<'123456'>
@@ -324,7 +395,9 @@ type DeepReadonly<Obj extends Record<string, any>> = Obj extends never
 
 type deepReadonly = DeepReadonly<{ A: { B: { C: { D: '3' } } } }>
 
-type ReverseStr<Str extends string> = Str extends `${infer First}${infer Rest}` ? `${ReverseStr<Rest>}${First}` : Str
+type ReverseStr<Str extends string> = Str extends `${infer First}${infer Rest}`
+  ? `${ReverseStr<Rest>}${First}`
+  : Str
 type ReverseStrResult = ReverseStr<'hello'>
 
 /**
@@ -332,7 +405,10 @@ type ReverseStrResult = ReverseStr<'hello'>
  */
 
 // 加法
-type Add<Num1 extends number, Num2 extends number> = [...BuildArray<Num1>, ...BuildArray<Num2>]['length']
+type Add<Num1 extends number, Num2 extends number> = [
+  ...BuildArray<Num1>,
+  ...BuildArray<Num2>
+]['length']
 
 /**
  * 元组和数组的区别：
@@ -344,19 +420,30 @@ type Add<Num1 extends number, Num2 extends number> = [...BuildArray<Num1>, ...Bu
  */
 
 // 减法  被减-减数 = 差 === Num1 -Num2 = 差
-type Subtract<Num1 extends number, Num2 extends number> = BuildArray<Num1> extends [...BuildArray<Num2>, ...infer Rest]
+type Subtract<
+  Num1 extends number,
+  Num2 extends number
+> = BuildArray<Num1> extends [...BuildArray<Num2>, ...infer Rest]
   ? Rest['length']
   : never
 
 type subtract = Subtract<31, 21>
 
 // 乘法 相当于是在加法的基础上连续加, 添加了一个 ResultArr 用来记录中间的结果
-type Mutiply<Num1 extends number, Num2 extends number, ResultArr extends unknown[] = []> = Num2 extends 0
+type Mutiply<
+  Num1 extends number,
+  Num2 extends number,
+  ResultArr extends unknown[] = []
+> = Num2 extends 0
   ? ResultArr['length']
   : Mutiply<Num1, Subtract<Num2, 1>, [...BuildArray<Num1>, ...ResultArr]>
 
 // 除法 递归的累积递减 每运行一次就会 +1
-type Divide<Num1 extends number, Num2 extends number, ResultArr extends unknown[] = []> = Num1 extends 0
+type Divide<
+  Num1 extends number,
+  Num2 extends number,
+  ResultArr extends unknown[] = []
+> = Num1 extends 0
   ? ResultArr['length']
   : Divide<Subtract<Num1, Num2>, Num2, [unknown, ...ResultArr]>
 
@@ -367,7 +454,10 @@ type divide = Divide<10, 2>
 // ---- 字符串
 
 // 求字符串的长度
-type Strlength<Str extends string, CountArr extends unknown[] = []> = Str extends `${infer Frist}${infer Rest}`
+type Strlength<
+  Str extends string,
+  CountArr extends unknown[] = []
+> = Str extends `${infer Frist}${infer Rest}`
   ? Strlength<Rest, [...CountArr, unknown]>
   : CountArr['length']
 
@@ -376,7 +466,11 @@ type strlength = Strlength<'length'>
 // ---- 数字
 
 // 数值大小的比较
-type GreaterThan<Num1 extends number, Num2 extends number, CountArr extends unknown[] = []> = Num1 extends Num2
+type GreaterThan<
+  Num1 extends number,
+  Num2 extends number,
+  CountArr extends unknown[] = []
+> = Num1 extends Num2
   ? false
   : CountArr['length'] extends Num2
   ? true
@@ -399,7 +493,12 @@ type FibonacciLoop<
   Num extends number = 1
 > = IndexArr['length'] extends Num
   ? CurrentArr['length']
-  : FibonacciLoop<CurrentArr, [...PrevArr, ...CurrentArr], [...IndexArr, unknown], Num>
+  : FibonacciLoop<
+      CurrentArr,
+      [...PrevArr, ...CurrentArr],
+      [...IndexArr, unknown],
+      Num
+    >
 
 type Fibonacci<Num extends number> = FibonacciLoop<[1], [], [], Num>
 
@@ -433,7 +532,7 @@ type StrUnion = `${Union}___===`
 
 // 判断联合类型
 type IsUnion<A, B = A> = A extends A ? ([B] extends [A] ? false : true) : never
-type isUnion = IsUnion<'a' | 'b' | 'c'>
+type isUnion = IsUnion<['a' | 'b' | 'c']>
 
 /**
  * 条件类型中如果左边的类型是联合类型，会把每个元素单独传入做计算，而右边不会。
@@ -461,11 +560,19 @@ type BEM<
   Modifiers extends string[]
 > = `${Block}__${Element[number]}--${Modifiers[number]}`
 
-type bemResult = BEM<'div', ['left', 'content', 'right'], ['content', 'color', 'display']>
+type bemResult = BEM<
+  'div',
+  ['left', 'content', 'right'],
+  ['content', 'color', 'display']
+>
 
 // 全组合
 
-type Combination<A extends string, B extends string> = A | B | `${A}${B}` | `${B}${A}`
+type Combination<A extends string, B extends string> =
+  | A
+  | B
+  | `${A}${B}`
+  | `${B}${A}`
 
 type AllCombinations<A extends string, B extends string = A> = A extends A
   ? Combination<A, AllCombinations<Exclude<B, A>>>
@@ -477,4 +584,128 @@ type AllCombinations<A extends string, B extends string = A> = A extends A
 
 // ==== 特殊类型
 
-//
+// 如何判断 类型是否是 any
+
+/**
+ * any 类型的特性
+ *
+ * any 类型与任何类型的交叉都是 any ，任何类型都是 any 类型 any 又可以是任何类型
+ *
+ * any 在联合类型中比较特殊，如果类型参数时 any 时候就会
+ * 返回 TrueType 和 FalseType 合并后的联合类型
+ *
+ */
+type isAny<T> = 'dong' extends 'wwww' & T ? true : false
+
+// 判断是否全等 这里就排除了 any 类型
+// type isEqualPlus<A, B> = (<T>()  => T extends A ? 1 : 2) extends <T>() => T extends B ? 1 : 2 ? true : false
+type isEqualPlus<A, B> = (<T>() => T extends A ? 1 : 2) extends <
+  T
+>() => T extends B ? 1 : 2
+  ? true
+  : false
+
+type TsEqualPlus = isEqualPlus<1, 2>
+
+/**
+ * 原理： 源码中这样写道 T1 extends X1 : X2 和 T2 extends U1 : U2
+ * 当 X1 和 U1 对应 X2 和 U2 对应 那么 T1 和 T2 对应
+ */
+
+// 判断 union （联合）类型
+
+/**
+ * union 类型遇到条件类型时就会分散成单个传入的计算的特性
+ */
+
+type IsUnionPlue<A, B = A> = A extends A
+  ? [B] extends [A]
+    ? false
+    : true
+  : never
+// 原理在上面的 IsUnion 中介绍， 这里需要注意的是 ？ 后的 [A] 其实是上次处理后的A 也就是传入联合类型 'a' | 'b' 中 a 或者 B
+
+// 判断 never 类型
+
+/**
+ * 原理：由于当 never 类型传入类型参数时就会导致直接返回never
+ * type IsNever<T> = T extends true ? 1 : 2
+ * 所以这里要处理一下
+ */
+type IsNever<T> = [T] extends [never] ? true : false
+
+// 判断是否是元组
+
+/**
+ * 元组中的每一个都是只读类型 readonly
+ * 元组和数组最大的不同是他们的 length 前者是字面量类型 后者是数字
+ */
+type IsTuple<T> = T extends readonly [...infer Eles]
+  ? NotEqual<Eles['length'], number>
+  : false
+
+type NotEqual<A, B> = (<T>() => T extends A ? 1 : 2) extends <
+  T
+>() => T extends B ? 1 : 2
+  ? true
+  : false
+
+// ==== 协变、逆变
+/**
+ * 需要明确的是 什么是协变 什么是逆变
+ * 类型之间可以相互有相同的部分这个时候就分为子类型和父类型
+ *
+ * 子类型相对于父类型来说更加具体 比如
+ * inter face {
+ *  a: number,
+ *  b: string
+ * }
+ *
+ * inter face_1 {
+ *  a: number,
+ *  b: string,
+ *  w: number,
+ *  e: boolean
+ * }
+ * face_1 显然是 face 的子类型
+ *
+ * 了解这点后在向看
+ *
+ * 当子类型给父类型的时候就是协变
+ *
+ * 父类型给子类型的时候就是逆变
+ *
+ * let a: face = {a: 1, b: 'b'}
+ * let b: face_1 = {a: 1, b: 'b', w: 1, e: true}
+ *
+ * a = b
+ * 这个时候就产生了协变 当子类型可以赋值给父类的时候就会产生协变
+ *
+ * 两者都能发生，就叫做双向协变
+ *
+ * 双向协变 存在于 ts 2x 版本中需要通过 strictFunctionTypes 来去管理
+ * strictFunctionTypes: boolean 默认 false
+ * ture 只支持函数参数的逆变，设置为 fasle 则是双向协变
+ *
+ * 需要注意：双向协变是有问题的，尽量避免
+ *
+ */
+
+/**
+ * 函数的参数具有逆变性质，而返回值是协变的
+ *
+ * 那么在函数传入参数和返回值的处理的时候就要额外处理
+ */
+
+type Func = (a: string) => void
+
+const func: Func = (a: 'hello') => undefined
+
+type UnionToIntersection<U> = (
+  U extends U ? (x: U) => unknown : never
+) extends (x: infer R) => unknown
+  ? R
+  : never
+type UnionToIntersecResult = UnionToIntersection<{ guang: 1 } | { dong: 2 }>
+
+// https://juejin.cn/book/7047524421182947366/section/7087183755504255016
